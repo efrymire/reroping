b9863d81436b969c3e2d4562a6ce60e39d7af8eb
Ellie Frymire <efrymire@netflix.com>
2025-06-06 12:50:06 -0700

    init commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+    <div style={{ height: "100%", width: "100%", display: "flex", flexDirection: "column", alignItems: "center", justifyContent: "center" }}>
+      <img src="src/assets/reroping.svg" alt="Reroping Logo" style={{ width: "300px", height: "300px" }} />
+    </div>
+  )
+}
+
+export default App
