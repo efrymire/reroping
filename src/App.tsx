7ded36465c037a6c06ba9398ec2fa8c4b2dc08eb
Ellie Frymire <efrymire@netflix.com>
2025-06-06 16:40:41 -0700

    small push trigger

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
       <img src={reropingLogo} alt="Reroping Logo" />
       <div className="details">
         <div>
-        Created for the 2025 SF Design Week theme Reform, the reroping piece explores transformation through craft and context, highlighting the potential of material to be reimagined and redefined.
+        Created for the 2025 SF Design Week theme "Reform", the reroping piece explores transformation through craft and context, highlighting the potential of material to be reimagined and redefined.
         </div>
         <div>Designed and curated by Ana Illanes in collaboration with women artisans in Chile. Handwoven using reclaimed fishing rope.
         </div>
